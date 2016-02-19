ca4de9549e09cf042de26d42a571b8f1f7e15050
anish19 <hoopanish@gmail.com>
2016-02-18 23:31:52 -0500

    kth smallest element added

--- tree.cpp
+++ tree.cpp
@@ -12,6 +12,7 @@ class TreeNode{
 		void set_left(TreeNode*);
 		void set_right(TreeNode*);
 		void set_lcount(int);
+		void print_node();
 		TreeNode *next;
 
 	private:
@@ -63,29 +64,56 @@ void TreeNode::set_lcount(int val){
 	this->lcount = val;
 }
 
+void TreeNode::print_node(){
+	if(this->get_left() != NULL && this->get_right() != NULL){
+		cout<<"[{node: "<<this->get_val()<<"}"<<
+			"	{l: "<<this->get_left()->get_val()<<
+			" r: "<<this->get_right()->get_val()<<"}"<<
+			"	{lcount: "<<this->get_lcount()<<"}]"<<endl;
+	}
+	else if(this->get_left() != NULL){
+			cout<<"[{node: "<<this->get_val()<<"}"<<
+				"	{l: "<<this->get_left()->get_val()<<
+				" r: NULL}"<<
+				"	{lcount: "<<this->get_lcount()<<"}]"<<endl;
+	
+	}
+	else if (this->get_right() != NULL){
+			cout<<"[{node: "<<this->get_val()<<"}"<<
+				"	{l: NULL"<<
+				" r: "<<this->get_right()->get_val()<<"}"<<
+				"	{lcount: "<<this->get_lcount()<<"}]"<<endl;
+	
+	
+	}
+	else{
+		cout<<"[{node: "<<this->get_val()<<"}]"<<endl;	
+	}
+}
+
 class Tree{
 	public:
 		Tree();
 		~Tree();
 		TreeNode* insert(int);
 		void print();
-		int lcount;
+		int node_count;
 //		vector<int> in_order_tr(TreeNode*);
-//		int kth_smallest(int);
+		TreeNode* kth_smallest(int);
 //		int kth_largest(int);
 	private:
 		void tree_insert(TreeNode*, TreeNode*);
+		TreeNode* find_k_sm(TreeNode*, int);
 		TreeNode* head, *tail;
 };
 
 Tree::Tree(){
-	head = NULL;
-	tail = NULL;
-	lcount = 0;
+	this->head = NULL;
+	this->tail = NULL;
+	this->node_count = 0;
 }
 
 void Tree::tree_insert(TreeNode* head, TreeNode* node){
-
 	if(head->get_val() > node->get_val()){
 		if(head->get_left() == NULL){
 			head->set_lcount(head->get_lcount()+1);
@@ -111,6 +139,7 @@ void Tree::tree_insert(TreeNode* head, TreeNode* node){
 TreeNode* Tree::insert(int val){
 	TreeNode *node = new TreeNode(val);
 	node->set_lcount(0);
+	this->node_count++;
 	if(this->head == NULL){
 		this->head = node;
 		node->set_left(NULL);
@@ -156,12 +185,27 @@ void Tree::print(){
 	printTree(this->head);
 }
 
+TreeNode* Tree::find_k_sm(TreeNode* node, int k){
+	if(node->get_lcount() == k)
+		return node;
+	else if (node->get_lcount() > k){
+		cout<<node->get_val()<<endl;
+		return find_k_sm(node->get_left(), k);
+	}
+	else
+		return find_k_sm(node->get_right(), k - node->get_lcount() -1);
+}
+
+TreeNode* Tree::kth_smallest(int k){
+	return find_k_sm(this->head, k);
+}
+
 int main(int argc, char **argv){
-	int t_size, temp;
-	cout<<"enter no of elements you want "<<endl;
+	int t_size, temp, k;
+	cout<<"enter no of elements you want: "<<endl;
 	cin>>t_size;
-	cout<<"no of elements "<<t_size<<endl;
-	cout<<"enter elements"<<endl;
+	cout<<"no of elements is "<<t_size<<endl;
+	cout<<"enter elements: (e.g. 3 21 4 83 0 -2 1)"<<endl;
 	Tree *t = new Tree();
 	for(int i = 1 ; i <= t_size; i ++){
 		cin>>temp;
@@ -170,6 +214,17 @@ int main(int argc, char **argv){
 	cout<<"\n\n Tree:"<<endl;
 	t->print();
 	cout<<"\nFormat:\n [{node}\t{children}\t{left_count}]"<<endl;
+	cout<<"\n\nEnter k to get the kth smallest element: ";
+	cin>>k;
+	while(k > t->node_count || k < 1){
+		cout<<"Tree only has "<< t->node_count<<
+		" elements.\nEnter a positive value less than or equal to "<<
+		t->node_count<<endl<<endl;
+		cin>>k;
+	}
+	cout<<"\n"<<k<<" smallest element is: ";
+	t->kth_smallest(k-1)->print_node();
+	cout<<endl;
 //	t.in_order_tr();
 	
 }
